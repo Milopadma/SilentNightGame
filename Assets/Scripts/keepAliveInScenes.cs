7283603febce9504355f124f26136fd4c90456d4
milo <48256865+Milopadma@users.noreply.github.com>
2022-08-05 15:18:43 +0800

    commit #12
    updates

--- Assets/Scripts/keepAliveInScenes.cs
+++ Assets/Scripts/keepAliveInScenes.cs
@@ -1,82 +1,14 @@
  using System.Collections.Generic;
  using UnityEngine;
  using UnityEngine.SceneManagement;
- 
- //the basis of this script was taken from keszeial, link to their post is in the credits scene and https://answers.unity.com/questions/1260393/make-music-continue-playing-through-scenes.html
- //the dictionary functionality is mine
+
  //signed //I GUSTI BAGUS MILO PADMA WIJAYA
- 
- /// <summary>
- /// Attach this component to objects that you want to keep alive (e.g. theme songs) in certain scene transitions. 
- /// For reusability, this component uses the scene names as strings to decide whether it survives or not after a scene is loaded 
- /// </summary>
+
  public class keepAliveInScenes : MonoBehaviour
  {
-     [Tooltip("Names of the scenes this object should stay alive in when transitioning into")]
-     public List<string> sceneNames;
- 
-     [Tooltip("A unique string identifier for this object, must be shared across scenes to work correctly")]
-     public string instanceName;
- 
-     public static Dictionary<string, string> _data = new Dictionary<string, string>();
-
-     // for singleton-like behaviour: we need the first object created to check for other objects and delete them in the scene during a transition
-     // since Awake() callback preceded OnSceneLoaded(), place initialization code in Start()
-     private void Start()
-     {
-        //reseet Dictionary
-        _data.Clear();
-        Debug.Log("(GAME STARTED)keepAliveInScenes.cs -- Dictionary Cleared");
-         DontDestroyOnLoad(this.gameObject);
- 
-         // subscribe to the scene load callback
-         SceneManager.sceneLoaded += OnSceneLoaded;
-     }
- 
-     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-     {
-         // delete any potential duplicates that might be in the scene already, keeping only this one 
-         CheckForDuplicateInstances();
- 
-         // check if this object should be deleted based on the input scene names given 
-         CheckIfSceneInList();
-     }
- 
-     void CheckForDuplicateInstances()
-     {
-         // cache all objects containing this component
-         keepAliveInScenes[] collection = FindObjectsOfType<keepAliveInScenes>();
- 
-         // iterate through the objects with this component, deleting those with matching identifiers
-         foreach (keepAliveInScenes obj in collection)
-         {
-             if(obj != this) // avoid deleting the object running this check
-             {
-                 if (obj.instanceName == instanceName)
-                 {
-                     Debug.Log("Duplicate object in loaded scene, deleting now...");
-                     DestroyImmediate(obj.gameObject);
-                 }
-             }
-         }
-     }
- 
-     void CheckIfSceneInList()
-     {
-         // check what scene we are in and compare it to the list of strings 
-         string currentScene = SceneManager.GetActiveScene().name;
- 
-         if (sceneNames.Contains(currentScene))
-         {
-             // keep the object alive 
-         }
-         else
-         {
-             // unsubscribe to the scene load callback
-             SceneManager.sceneLoaded -= OnSceneLoaded;
-             DestroyImmediate(this.gameObject);
-         }
-     }
+     public static Dictionary<string, string> _data = new Dictionary<string, string>(); //the dictionary of data
+     public string instanceName; //the name of the instance of this script
+     public List<string> sceneNames; //list of scene names to keep alive
      //add data to the dictionary
      public void AddData(string key, string value)
      {
@@ -95,5 +27,41 @@
          if (_data.ContainsKey(key)){_data[key] = value;}
      }
 
+     private void Start()
+     {
+        _data.Clear(); //reset Dictionary
+        Debug.Log("(GAME STARTED)keepAliveInScenes.cs -- Dictionary Cleared");
+         DontDestroyOnLoad(this.gameObject); // make this object persistent
+         SceneManager.sceneLoaded += OnSceneLoaded; //subscribe to the event of sceneloads from scene manager
+     }
+
+     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+     {
+         duplicateInstanceCheck(); //check if there is another instance of this script
+         sceneinListCheck(); //check if the scene is in the list of scenes to keep alive
+     }
+
+     void duplicateInstanceCheck()
+     {
+         keepAliveInScenes[] collection = FindObjectsOfType<keepAliveInScenes>(); //get all instances of this script
+         foreach (keepAliveInScenes obj in collection) //loop through the collection of keepAliveInScene instances
+         {
+             if(obj != this) //if the object is not this instance
+             {
+                 if (obj.instanceName == instanceName) // if the instance name is equals to the instanceName
+                 {DestroyImmediate(obj.gameObject);} //destroy the duplicate instance
+             }
+         }
+     }
+
+     void sceneinListCheck()
+     {
+         string currentScene = SceneManager.GetActiveScene().name; //get the name of the current scene
+         if (!(sceneNames.Contains(currentScene))) //if the current scene is not in the list of scenes to keep alive
+         {
+            SceneManager.sceneLoaded -= OnSceneLoaded; //unsubscribe from the event of sceneloads from scene manager
+            DestroyImmediate(this.gameObject); //destroy this instance
+         }
+     }
  }
  
